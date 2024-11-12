ee9cb1155a2c18f47cedf1da01e35f63b0189436
AnderVicario <102811194+AnderVicario@users.noreply.github.com>
2024-11-12 16:05:30 +0100

    feat: create ej2main.py

---
+++ ej2main.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+    """Función que devuelve la suma de dos números."""
+    return a + b
+
+# Variables de ejemplo
+num1 = 5
+num2 = 10
+
+# Mostrar el resultado de la suma
+print(f"La suma de {num1} y {num2} es: {suma(num1, num2)}")
